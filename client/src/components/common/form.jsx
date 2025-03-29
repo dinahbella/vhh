4ce995e00c0bfa1a08a238799c24a5a72f25d755
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-29 15:19:54 +0100

    register

--- client/src/components/common/form.jsx
+++ client/src/components/common/form.jsx
@@ -1,9 +1,124 @@
 import React from "react";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
 
-export default function Form() {
+export default function Form({
+  formControls,
+  formData,
+  setFormData,
+  onSubmit,
+  buttonText,
+}) {
+  const renderInputByComponentType = (getControlItem) => {
+    const value = formData[getControlItem.name || ""];
+
+    switch (getControlItem.componentType) {
+      case "input":
+        return (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+            value={value}
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: e.target.value,
+              })
+            }
+          />
+        );
+
+      case "select":
+        return (
+          <Select
+            value={value}
+            onValueChange={(value) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: value,
+              })
+            }
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder={getControlItem.placeholder} />
+            </SelectTrigger>
+            <SelectContent>
+              {getControlItem.options && getControlItem.options.length > 0
+                ? getControlItem.map((optionItem) => (
+                    <SelectItem key={optionItem.id} value={optionItem.id}>
+                      {optionItem.label}
+                    </SelectItem>
+                  ))
+                : null}
+            </SelectContent>
+          </Select>
+        );
+
+      case "textarea":
+        return (
+          <Textarea
+            name={getControlItem.name}
+            id={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            className="w-full border px-3 py-2 rounded"
+            rows={getControlItem.rows || 4}
+            value={value}
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: e.target.value,
+              })
+            }
+          />
+        );
+
+      default:
+        return (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+            value={value}
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: e.target.value,
+              })
+            }
+          />
+        );
+    }
+  };
+
   return (
     <div>
-      <form action=""></form>
+      <form action="" onSubmit={onSubmit}>
+        <div className="flex flex-col gap-3">
+          {formControls.map((controlItem) => (
+            <div className="w-full grid gap-1.5" key={controlItem.name}>
+              <Label className="mb-1" htmlFor={controlItem.name}>
+                {controlItem.label}
+              </Label>
+              {renderInputByComponentType(controlItem)}
+            </div>
+          ))}
+        </div>
+        <Button type="submit" className="mt-3 hover:bg-green-900 w-full">
+          {buttonText || "Submit"}
+        </Button>
+      </form>
     </div>
   );
 }
--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,10 +1,43 @@
 import AuthLayout from "@/components/auth/layout";
-import React from "react";
+import Form from "@/components/common/form";
+import { RegisterFormControls } from "@/config";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const initialState = {
+  userName: "",
+  email: "",
+  password: "",
+};
 
 export default function Register() {
+  const [formData, setFormData] = useState(initialState);
+  const onSubmit = () => {};
   return (
     <AuthLayout>
-      <div>Register</div>
+      <div className="mx-auto w-full max-w-md space-y-6">
+        <div className="text-center">
+          <h1 className=" text-3xl font-bold tracking-tight text-foreground">
+            Create an account
+          </h1>
+          <p className="mt-2 ">
+            Already have an account?
+            <Link
+              className="text-primary font-bold ml-2 hover:underline"
+              href={"/auth/login"}
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+        <Form
+          formControls={RegisterFormControls}
+          buttonText={"Sign Up"}
+          formData={formData}
+          setFormData={setFormData}
+          onSubmit={onSubmit}
+        />
+      </div>
     </AuthLayout>
   );
 }
