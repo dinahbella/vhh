1ec58892bd264fde8757b7eb4df9c5f5441d7ffc
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-04-09 16:50:01 +0100

    address

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,12 +3,14 @@ import authReducer from "./auth-slice";
 import adminProductSlice from "./admin/productSlice";
 import shopProductSlice from "./shop/productSlice";
 import shopCartSlice from "./shop/cartSlice";
+import addressSlice from "./shop/addressSlice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: adminProductSlice,
     shopProducts: shopProductSlice,
     shopCart: shopCartSlice,
+    address: addressSlice,
   },
 });
 
